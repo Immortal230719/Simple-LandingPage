2cdb60d71dd705c5f7a5cc90e19a8ce543e4868a
Immortal230719 <golub010919@mail.ru>
2020-03-05 22:20:01 +0400

    section products begining with nav_fixed

---
+++ src/js/main.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+  //! navFixed fadeIn
+
+  let windowWidth = window.innerWidth;
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 150 && windowWidth > 576) {
+      $("nav_fixed").hide();
+      $(".nav_fixed").fadeIn();
+    } else {
+      $(".nav_fixed").fadeOut();
+    }
+  });
+
+  //! slider
+
+  $("#slider").slick({
+    infinite: true,
+    slidesToShow: 1,
+    arrows: true,
+    variableWidth: true,
+    slidesToScroll: 1,
+    dotsClass: "my-dots",
+    dots: true,
+    speed: 1000,
+    centerMode: true,
+    prevArrow: '<button type="button" class="slick-prev"><</button>',
+    nextArrow: '<button type="button" class="slick-next">></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          arrows: false
+        }
+      }
+    ]
+  });
+
+  //! pageUp button
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 400) {
+      $(".page-up").fadeIn();
+    } else {
+      $(".page-up").fadeOut();
+    }
+  });
+
+  $("a[href^=#up]").click(function() {
+    const _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+  });
+
+  new WOW().init();
+});
