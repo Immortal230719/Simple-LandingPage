3eaa93a92fd8188338dbfe46d1679c3cc3b3d5cb
Immortal230719 <golub010919@mail.ru>
2020-03-06 18:11:15 +0400

    w3c validated, fotter section begining, section good added, section products done

--- src/js/main.js
+++ src/js/main.js
@@ -49,7 +49,7 @@ $(document).ready(function() {
     }
   });
 
-  $("a[href^=#up]").click(function() {
+  $("#page-up").click(function() {
     const _href = $(this).attr("href");
     $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
     return false;
